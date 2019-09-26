191b8d7b12110bf67662e4fece00a0d6627de84c
Jozef-Sz <joccopalica@gmail.com>
2019-09-26 21:47:21 +0200

    load dict

--- spell checker/content.js
+++ spell checker/content.js
@@ -1,5 +1,72 @@
 const url = chrome.runtime.getURL('./data/sk_SK.dic');
+let dict;
 
 fetch(url)
     .then(response => response.text())
-    .then(json => console.log(json));
+    .then(dic => dict = dic)
+    .then(() => main());
+
+function main() {
+    const spell = parseDic(dict);
+    console.log(spell);
+    
+}
+
+function parseDic(data) {
+    let arr = [];
+
+    const buffering = new Buffer();
+   
+    // We start from 7, cuz we're ignoring the first unnecessary element 
+    for (let i = 7; i < dict.length; i++) {
+        if (dict[i] === "\n") {
+            arr.push({
+                "word": buffering.getWord(),
+                "flag": buffering.getFlag()
+            });
+            buffering.clear();
+        } else {
+            if (dict[i] === "/") {
+                buffering.flagDetected();
+                continue;
+            }
+            buffering.add(dict[i]);
+        }
+    }
+
+    return arr;
+}
+
+class Buffer {
+    constructor() {
+        this.wordBuffer = "";
+        this.flagBuffer = "";
+        this.flagActive = false;
+    }
+
+    add(token) {
+        if (!this.flagActive) {
+            this.wordBuffer += token;
+        } else {
+            this.flagBuffer += token;
+        }
+    }
+
+    flagDetected() {
+        this.flagActive = true;
+    }
+
+    clear() {
+        this.wordBuffer = "";
+        this.flagBuffer = "";
+        this.flagActive = false;
+    }
+
+    getWord() {
+        return this.wordBuffer;
+    }
+
+    getFlag() {
+        return this.flagBuffer;
+    }
+}
