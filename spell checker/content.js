62a62c6c273d3535dc471f10ea525eb324558297
Jozef-Sz <joccopalica@gmail.com>
2019-09-27 11:00:38 +0200

    added text loading

--- spell checker/content.js
+++ spell checker/content.js
@@ -1,4 +1,5 @@
 const url = chrome.runtime.getURL('./data/sk_SK.dic');
+let readyToCheck = false;
 let dict;
 
 fetch(url)
@@ -8,9 +9,33 @@ fetch(url)
 
 function main() {
     const spell = parseDic(dict);
-    console.log(spell);
     
+    const paragraphs = document.getElementsByTagName('p');
+    let content = new Array();
+    for (let p of paragraphs) {
+        content.push(new VirtualParagraph(p));
+    }
+
+    readyToCheck = true;
+
+
+    // Just for debugging
+    for (let i = 0; i < paragraphs.length; i++) {
+        content[i].check();
+        paragraphs[i].innerHTML = content[i].getNewP();
+    }
 }
+
+function spellCheck(paragraphs, content) {
+    /* This function will be triggered by the button up in corner of the browser from background script */
+    if (readyToCheck){
+        for (let i = 0; i < paragraphs.length; i++) {
+            content[i].check();
+            paragraphs[i].innerHTML = content[i].getNewP();
+        }
+    }
+}
+
 
 function parseDic(data) {
     let arr = [];
@@ -35,6 +60,38 @@ function parseDic(data) {
     }
 
     return arr;
+}
+
+class VirtualParagraph {
+    constructor(paragraph)  {
+        this.pInnerHTML = paragraph.innerHTML;
+        this.currentIndex = 0;
+        this.isIgnoring = false;
+        this.newP;
+    }
+
+    check() {
+        // for (let i = 0; i < this.pInnerHTML.length; i++) {
+        //     console.log(this.pInnerHTML[i]);
+        // }
+        console.log(this.pInnerHTML);
+        console.log(this.pInnerHTML[0]);
+        console.log(this.pInnerHTML[1]);
+        console.log(this.pInnerHTML[2]);
+        console.log(this.pInnerHTML[3]);
+        console.log(this.pInnerHTML[4]);
+        console.log(this.pInnerHTML[5]);
+        console.log(this.pInnerHTML[6]);
+        console.log(this.pInnerHTML[7]);
+        console.log(this.pInnerHTML[8]);
+        console.log(this.pInnerHTML[9]);
+        console.log(this.pInnerHTML[10]);
+        this.newP = this.pInnerHTML + '<span class="highlight">Added text</span>';
+    }
+
+    getNewP() {
+        return this.newP;
+    }
 }
 
 class Buffer {
