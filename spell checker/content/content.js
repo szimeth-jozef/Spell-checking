0a02e76c76e05de488144be2f3faa6d2c1126168
Jozef-Sz <joccopalica@gmail.com>
2019-11-13 12:52:34 +0100

    Improved text search method and error highlighting insertion

--- spell checker/content/content.js
+++ spell checker/content/content.js
@@ -33,16 +33,13 @@ function onload(dictionary) {
     
     const body = document.querySelectorAll('body *');
 
-    const filteredElements = tagFilter(body);
-    console.log(filteredElements);
-    const node1 = filteredElements[0].childNodes[0];
-    const node2 = filteredElements[0].childNodes[2];
-    console.log(node1.isSameNode(node2));
-    console.log(node1.isSameNode(node1));
-    debugger
+    const filteredElements = preFilter(body);
+    const textNodes = filteredElements.textNodeFilter();
+    console.log(textNodes);
+    // textNodes.forEach(node => console.log(node.textContent))
 
-    for (let element of filteredElements) {
-        VirtualElementHolder.push(new VirtualElement(element));
+    for (let textNode of textNodes) {
+        VirtualElementHolder.push(new VirtualElement(textNode));
     }
 
     // After everything is loaded, the dictionary, elements and other stuff we can enable the button
@@ -94,11 +91,10 @@ function parseDic(data) {
  * @param {NodeList} tags - array of nodes
  * @returns {NodeList} - filtered nodes list
  */
-function tagFilter(tags) {
+function preFilter(tags) {
     let newTags = new Array();
 
     for (const node of tags) {
-        // TODO: check text node 
         if (blackListTags.includes(node.tagName)) {
             continue;
         }
@@ -116,6 +112,42 @@ function tagFilter(tags) {
 }
 
 /**
+ * @description - Filters out TextNodes from the array of HTML elements 
+ * @returns {Array<Node>} - Array of TextNodes
+ */
+Array.prototype.textNodeFilter = function() {
+    let textNodes = [];
+    for (const element of this) {
+        if (element.nodeType === Node.TEXT_NODE && element.nodeValue.trim().length !== 0) {
+            textNodes = addNode(element, textNodes);
+        }
+        for (const childNode of element.childNodes) {
+            if (childNode.nodeType === Node.TEXT_NODE && childNode.nodeValue.trim().length !== 0) {
+                textNodes = addNode(childNode, textNodes);
+            }
+        }
+    }
+    return textNodes;
+}
+
+/**
+ * @description - Add node to an array if the array does not consist the given node yet
+ * @param {Node} node - Node element which we intend add to the array 
+ * @param {Array<Node>} nodeArray - Array which we want to add the node element 
+ */
+function addNode(node, nodeArray) {
+    for (let i = 0; i < nodeArray.length; i++) {
+        if (nodeArray[i].isSameNode(node)) {
+            return nodeArray;
+        }
+    }
+    nodeArray.push(node);
+    return nodeArray;
+}
+
+
+
+/**
  * @description - Function to turn misspeling highlight on or off
  * @param {boolean} state - Decide whether highlighting should be turned off or on
  */
@@ -130,147 +162,5 @@ function turnHighlight(state) {
         for (let i = spans.length - 1; i >= 0; i--) {
             spans[i].className = 'emptyClassHolder';
         }
-    }
-}
-
-/**
- * @class
- * @description - (deprecated, need to be updated) VirtualElement gets innerText of one of the elements then from innerText it detects words 
-    and right after a word is detected it is compared with words in the dictionary. If a word is not matching, it
-    is probably misspelled so the word gets wrapped around span tags and it's added to newInnerHTML. The words are
-    added to newInnerHTML whether it's correct or misspelled otherwise the updated innerHTML of a paragraph
-    would be incomplete
- */
-class VirtualElement {
-
-    /**
-     * @param {HTMLElement} element - Raw HTML element
-     */
-    constructor(element)  {
-        this.node = element;
-        this.childNodes = element.childNodes;
-        this.eInnerHTML = element.innerHTML; // Not used
-        this.newInnerHTML = '';
-        this.highlighting = ['<span class="misspell-highlight-SCH-Extension">', '</span>'];
-    }
-
-    check() {
-        this.childNodes.forEach((node) => {
-            // Ez itt nem tetszik neki, szoval refactor
-            if (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim().length !== 0) {
-                console.log(node);
-                console.log(node.parentNode.innerHTML);
-                console.log("----------------------");
-
-                const words = node.nodeValue.split(/\s+/g);
-
-                for (let word of words) {
-                    if (word !== "") {
-                        // if result is false then the word is marked as misspelled
-                        let result = this.compare(this.getRidOfPunctuation(word));
-
-                        // this method below handles creating the updated paragraph
-                        this.addWord(result, word);
-                    }
-                }
-
-                this.applyChanges(node);
-            }
-        });
-    }
-
-    applyChanges(node) {
-        node.parentNode.innerHTML = this.newInnerHTML;
-        this.newInnerHTML = '';
-    }
-
-    /**
-     * @description - Add word to newInnerHTML with or without span tags 
-     * @param {boolean} res - Boolean value, whether wrap the word or not 
-     */
-    addWord(res, word) {
-        if (res) {
-            this.newInnerHTML += word + " ";
-        } else {
-            this.newInnerHTML += this.highlighting[0] + word + this.highlighting[1] + " ";
-        }
-    }
-
-    /**
-     * 
-     * @param {string} word - Word which is going to be tested
-     * @returns {boolean} - Depend on whether the passed word was found or not
-     */
-    compare(word) {
-        if (word in parsedDic || word.toLowerCase() in parsedDic) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * @description - Takes the current word and returns it without punctuation
-     * @returns {string} - Returns this.currentWord without punctuation
-     */
-    getRidOfPunctuation(word) {
-        // TODO: clean up the double replace and question mark problem
-        const regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
-        const questionMark = /\?/g;  
-        let clsWord = word.replace(regex, ""); 
-        clsWord = clsWord.replace(questionMark, "");
-        return clsWord; 
-    }
-
-    getNewInnerHTML() {
-        return this.newInnerHTML;
-    }
-}
-
-/**
- * @class
- * @description - Simple buffering system which accumulates letters and separates words from flags
- */
-class DictionaryBuffer {
-
-    constructor() {
-        this.wordBuffer = "";
-        this.flagBuffer = "";
-        this.flagActive = false;
-    }
-
-    /**
-     * @description - Add letter to buffer based on whether a flag is detected or not
-     * @param {string} token - Letter to add to buffer
-     */
-    add(token) {
-        if (!this.flagActive) {
-            this.wordBuffer += token;
-        } else {
-            this.flagBuffer += token;
-        }
-    }
-
-    /**
-     * @description - Enable flagActive mode inside of DictionaryBuffer
-     */
-    flagDetected() {
-        this.flagActive = true;
-    }
-
-    /**
-     * @description - Reset state of buffer
-     */
-    clear() {
-        this.wordBuffer = "";
-        this.flagBuffer = "";
-        this.flagActive = false;
-    }
-
-    getWord() {
-        return this.wordBuffer;
-    }
-
-    getFlag() {
-        return (this.flagBuffer == "") ? null : this.flagBuffer;
     }
 }
